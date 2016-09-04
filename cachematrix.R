cdd8aa83635f0a4c56dbf8b85737a2a4004ad3ab
kidomcl <donmclel@yahoo.com>
2016-09-04 15:38:44 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,22 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Initializes variables (m,y, also set, get, setmean, and getmean)
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x ) {
 
+        m <- NULL
+        set <- function(y) {
+                y=matrix(rbind(c(.5, .25), c(.25, .5)))
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
 
 
@@ -12,4 +24,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        c=rbind(c(1, -1/4), c(-1/4, 1))
+        
+        
+        if (identical(x,c)) c=c else x=c
+        ## Returns the identity matrix
+        solve(c)%*%c
+        ## Returns the inverse
+        solve(c)
 }
